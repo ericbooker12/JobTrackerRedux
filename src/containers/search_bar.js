ff853af857b84da8539737938ebe0c4dee97575f
Eric Booker <ericbooker12@gmail.com>
2017-12-03 18:33:25 -0800

    Initial commit. Add searchBar container

---
+++ src/containers/search_bar.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index'
+
+export default class SearchBar extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { term: ''}
+
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event){
+		this.setState({ term: event.target.value });
+	}
+
+	onFormSubmit(event){
+		event.preventDefault();
+
+		// We need to go fetch job data
+
+		// Reset the term to empty
+		this.setState({term: ''});
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.onFormSubmit} className='input-group'>
+				<input
+					placeholder='Enter search term'
+					className='form-control'
+					value={this.state.term}
+					onChange={this.onInputChange}/>
+				<span className="input-group-btn">
+					<button type='submit' className='btn btn-secondary'>Submit</button>
+				</span>
+
+			</form>
+		)
+	}
+}
+
+
+
+
+
+
+
+
